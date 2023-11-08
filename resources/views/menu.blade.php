42d82c0c1ff1a75390d83811415f8cedc58b0f76
Lyov Moroyan <lyov.moroyan03@gmail.com>
2023-11-08 18:08:29 +0400

    Fix menu design problem

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full h-auto flex relative">
+<div class="w-[110%] h-auto flex relative">
    <div class="w-[30%] h-[200px] bg-[#404338] ml-[-40px]" style="transform: skewX(20deg);">
    </div>
     <div class="w-[70%] h-[200px] " style="transform: skewX(20deg)">
